5edfce9e14fe42b6ca5838edfa8e2c2503b3210a
Will Larson <lethain@gmail.com>
2018-11-17 15:05:29 -0800

    Add server.py

--- tutorial/server.py
+++ tutorial/server.py
@@ -4,16 +4,16 @@ import requests
 
 
 class Website(graphene.ObjectType):
-    url = graphene.String()
+    url = graphene.String(required=True)
     title = graphene.String()
     description = graphene.String()
     image = graphene.String()
 
 class Query(graphene.ObjectType):
-    #website = graphene.Field(Website, url=graphene.String(required=True))
-    website = graphene.String(url=graphene.String(required=True))
+    website = graphene.Field(Website)
     
-    def resolve_website(self, info, url):
+    def resolve_website(self, info):
+        url = info.context.get('url')        
         print(['resolve_website', info, url])
         html = requests.get(url).text
         extracted = extraction.Extractor().extract(html, source_url=url)
@@ -25,5 +25,12 @@ class Query(graphene.ObjectType):
 
 
 schema = graphene.Schema(query=Query)
-results = schema.execute('{ website (url: "https://lethain.com/migrations/") }')
-print(results.data)
+result = schema.execute("""
+query getWebsite($url: URL) {
+  website(url: $url) {
+    url
+    title
+  }
+}""", variables={'url': "https://lethain.com/migrations/"})
+
+print(result)
