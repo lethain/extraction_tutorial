2ce80ab4726ff7a5e6fef7f6e3a6c13ecc793ae8
Will Larson <lethain@gmail.com>
2018-11-18 10:42:25 -0800

    Tweaks

--- extraction_tutorial/schema.py
+++ extraction_tutorial/schema.py
@@ -5,24 +5,30 @@ import graphene
 import extraction
 import requests
 
+def extract(url):
+    html = requests.get(url).text
+    extracted = extraction.Extractor().extract(html, source_url=url)
+    print(extracted)
+    return extracted
 
 class Website(graphene.ObjectType):
     url = graphene.String(required=True)
     title = graphene.String()
     description = graphene.String()
     image = graphene.String()
+    feed = graphene.String()
 
     
 class Query(graphene.ObjectType):
     website = graphene.Field(Website, url=graphene.String())
 
     def resolve_website(self, info, url):
-        html = requests.get(url).text
-        extracted = extraction.Extractor().extract(html, source_url=url)
-        return Website(url=extracted.url,
+        extracted = extract(url)
+        return Website(url=url,
                        title=extracted.title,
                        description=extracted.description,
-                       image=extracted.image
+                       image=extracted.image,
+                       feed=extracted.feed
         )
 
 schema = graphene.Schema(query=Query)
